83f533bd2962c53da47c59cfae8d0cfc47aadbeb
Sebastian Malton <sebastian@malton.name>
2020-11-05 10:24:13 -0500

    revert badge changes
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/badge/badge.tsx
+++ src/renderer/components/badge/badge.tsx
@@ -3,10 +3,6 @@ import "./badge.scss"
 import React from "react";
 import { cssNames } from "../../utils/cssNames";
 import { TooltipDecoratorProps, withTooltip } from "../tooltip";
-import { observer } from "mobx-react";
-import { autobind } from "../../utils";
-import { observable } from "mobx";
-import { Icon } from "../icon";
 
 interface Props extends React.HTMLAttributes<any>, TooltipDecoratorProps {
   small?: boolean;
@@ -14,48 +10,14 @@ interface Props extends React.HTMLAttributes<any>, TooltipDecoratorProps {
 }
 
 @withTooltip
-@observer
 export class Badge extends React.Component<Props> {
-  private elem: HTMLElement;
-
-  @observable isExpanded = false
-  @observable canBeExpanded = false
-
-  componentDidMount() {
-    this.canBeExpanded = this.elem.offsetWidth < this.elem.scrollWidth
-  }
-
-  @autobind()
-  onClick() {
-    if (this.canBeExpanded) {
-      this.isExpanded = !this.isExpanded
-    }
-  }
-
-  renderExpansionIcon() {
-    if (!this.canBeExpanded) {
-      return null
-    }
-
-    if (this.isExpanded) {
-      return <Icon className="expansionIcon" size={20} material="close_fullscreen" onClick={this.onClick} />
-    }
-
-    return <Icon className="expansionIcon" size={20} material="open_in_full" onClick={this.onClick} />
-  }
-
   render() {
     const { className, label, small, children, ...elemProps } = this.props;
-    const classNames = cssNames("Badge", className, { small, expanded: this.isExpanded })
-
-    return (
-      <div className={classNames} {...elemProps}>
-        {this.renderExpansionIcon()}
-        <div className="children" ref={ref => (this.elem = ref)}>
-          {label}
-          {children}
-        </div>
-      </div>
-    )
+    return <>
+      <span className={cssNames("Badge", { small }, className)} {...elemProps}>
+        {label}
+        {children}
+      </span>
+    </>
   }
 }
